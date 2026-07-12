7d815b2a40254c6c7996150411dfe46c961a5e46
Abdurahim <abdurahimbohodirov25@gmail.com>
2026-07-12 19:52:46 +0500

    1 commit: pom.xml + application.yml + BaseEntity + Enum ready

---
+++ src/main/java/org/example/ecommerce/entity/base/BaseEntity.java
@@ -0,0 +1,43 @@
+package org.example.ecommerce.entity.base;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.experimental.SuperBuilder;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+
+import java.time.LocalDate;
+
+@Getter
+@Setter
+@MappedSuperclass
+@NoArgsConstructor
+@AllArgsConstructor
+@SuperBuilder
+public abstract class BaseEntity  {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @CreatedDate
+    @Column(updatable = false, nullable = false)
+    private LocalDate createdAt;
+
+    @LastModifiedDate
+    @Column(nullable = false)
+    private LocalDate updatedAt;
+
+    @Column(nullable = false)
+    private boolean deleted = false;
+
+    private void delete(){
+        this.deleted = true;
+    }
+    public void restore() {
+        this.deleted = false;
+    }
+}
---
+++ src/main/java/org/example/ecommerce/enums/OrderStatus.java
@@ -0,0 +1,9 @@
+package org.example.ecommerce.enums;
+
+public enum OrderStatus {
+    PENDING,
+    CONFIRMED,
+    SHIPPED,
+    DELIVERED,
+    CANCELLED
+}
