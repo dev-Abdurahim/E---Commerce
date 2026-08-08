096d25a518ed3ff62b602a255d7dc8b3ab448247
Abdurahim <abdurahimbohodirov25@gmail.com>
2026-08-08 18:05:57 +0500

    6 commit: Repository Ready:

---
+++ src/main/java/org/example/ecommerce/repository/CartItemRepository.java
@@ -0,0 +1,14 @@
+package org.example.ecommerce.repository;
+
+import org.example.ecommerce.entity.CartItem;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface CartItemRepository extends JpaRepository<CartItem, Long> {
+
+    Optional<CartItem> findByCartIdAndProductId(Long cartId, Long productId);
+
+}
