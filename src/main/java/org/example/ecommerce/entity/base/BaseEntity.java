25999eb6b41c9811846b628984ae7a1a9fbcd7d4
Abdurahim <abdurahimbohodirov25@gmail.com>
2026-08-12 12:02:49 +0500

    9 commit: BaseEntity fix bugs:

--- src/main/java/org/example/ecommerce/entity/base/BaseEntity.java
+++ src/main/java/org/example/ecommerce/entity/base/BaseEntity.java
@@ -7,12 +7,14 @@ import lombok.NoArgsConstructor;
 import lombok.Setter;
 import org.springframework.data.annotation.CreatedDate;
 import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
 import java.time.LocalDate;
 
 @Getter
 @Setter
 @MappedSuperclass
+@EntityListeners(AuditingEntityListener.class)
 @NoArgsConstructor
 @AllArgsConstructor
 public abstract class BaseEntity  {
